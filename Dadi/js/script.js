043cee13bbe807c83fb5f37037c8029fe4b43a23
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-10-11 15:32:39 +0200

    Added bonus to dadi exercise

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -4,25 +4,41 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
 // DATI
-// Numero random utente
-const userNumber = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
-console.log(userNumber);
-
-// Numero random computer 
-const computerNumber = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
-console.log(computerNumber);
+const btnPlay = document.getElementById("btn-play");
+const userDice = document.getElementById("user-dice");
+const computerDice = document.getElementById("computer-dice");
+const resultBox = document.querySelector(".result");
 
 // ELABORAZIONE
-// controlliamo chi ha il punteggio più alto
-let winning = "";
+btnPlay.addEventListener("click", function(){
+    // Genero Numero random utente
+    const userNumber = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+    console.log(userNumber);
 
-if(userNumber > computerNumber){
-    winning = "L'Utente ha vinto";
-} else if(computerNumber > userNumber){
-    winning = "Il Computer ha vinto";
-} else {
-    winning = "Avete Pareggiato";
-}
+    // Genero Numero random computer 
+    const computerNumber = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+    console.log(computerNumber);
 
-// OUTPUT
-console.log(winning);
+    let winning = "";
+    let winStyle = "";
+
+    // controlliamo chi ha il punteggio più alto
+    if(userNumber > computerNumber){
+        winning = "L'Utente ha vinto";
+        winStyle = "win";
+    } else if(computerNumber > userNumber){
+        winning = "Il Computer ha vinto";
+        winStyle = "lose";
+
+    } else {
+        winning = "Avete Pareggiato";
+        winStyle = "draw";
+    }
+
+    // OUTPUT
+    console.log(winning);
+    userDice.innerHTML = `<h1>${userNumber}</h1>`;
+    computerDice.innerHTML = `<h1>${computerNumber}</h1>`;
+
+    resultBox.innerHTML = `<h1 class="${winStyle}">${winning}</h1>`;
+});
